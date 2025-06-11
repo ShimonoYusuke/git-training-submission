447f9e3f9a8f15e9eebfb19c395cfbff7180d1b1
user <user@DESKTOP-IOCFTSU>
2025-06-11 17:19:43 +0900

    j

--- ch1/src/ch1/Main1_1.java
+++ ch1/src/ch1/Main1_1.java
@@ -12,7 +12,7 @@ public class Main1_1 {
 		System.out.println("RPG:スッキリ魔王征伐");
 		System.out.println("Ver.0.1 by 湊 ");
 		System.out.println("＜ただいま鋭意学習・製作中＞");
-		System.out.println("プログラムを終了しますa");
+		System.out.println("プログラムを終了します");
 		//命令の部分
 	}
 }
